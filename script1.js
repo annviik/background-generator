36076fa4a5d677a5d5218b4b23f6b38cead4eb93
annviik <48365158+annviik@users.noreply.github.com>
2020-03-28 14:13:18 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -10,6 +10,12 @@ function setGradient() {
 	 +color2.value
 	 + ")";
 };
+function noSameColors() {
+if (color1.value === color2.value){
+	alert("Please choose different colors");
+}else 
+alert("Thank you for using background-generator");
+};
 // color1.addEventListener("input",function function_name(argument) {
 // 	body.style.background= "linear-gradient(to right, " 
 // 	 +color1.value
