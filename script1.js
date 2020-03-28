899407674946a86bc2d4fb35adbf55b9b39e628b
annviik <48365158+annviik@users.noreply.github.com>
2020-03-28 15:28:43 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -4,17 +4,23 @@ var color2=document.querySelector(".color2");
 var body=document.getElementById("gradient");
 
 function setGradient() {
+if(color1.value===color2.value){
+	alert("Please use different colors");
+}else {
 	 body.style.background= "linear-gradient(to right, " 
 	 +color1.value
 	 +", " 
 	 +color2.value
 	 + ")";
+	css.textContent = body.style.background + ";";}
 };
-function noSameColors() {
-if (color1.value === color2.value){
-	alert("Please choose different colors");
-}else 
-alert("Thank you for using background-generator");
+
+};
+// function noSameColors() {
+// if (color1.value === color2.value){
+// 	alert("Please choose different colors");
+// }else 
+// alert("Thank you for using background-generator");
 };
 // color1.addEventListener("input",function function_name(argument) {
 // 	body.style.background= "linear-gradient(to right, " 
